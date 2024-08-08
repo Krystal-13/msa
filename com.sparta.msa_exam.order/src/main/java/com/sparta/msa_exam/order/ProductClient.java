5e5811dd3973eb4011c7ed60840974ddab8fa6b3
krystal-13 <honey9163@gmail.com>
2024-08-08 21:26:00 +0900

    feat: ProductClient 구현

---
+++ com.sparta.msa_exam.order/src/main/java/com/sparta/msa_exam/order/ProductClient.java
@@ -0,0 +1,17 @@
+package com.sparta.msa_exam.order;
+
+import com.sparta.msa_exam.product.ProductResponseDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name = "product-service")
+public interface ProductClient {
+    @GetMapping("/products/{id}")
+    ProductResponseDto getProduct(@PathVariable("id") Long id);
+
+
+    @GetMapping("/products/{id}/reduceQuantity")
+    void reduceProductStock(@PathVariable("id") Long id, @RequestParam("quantity") int quantity);
+}
