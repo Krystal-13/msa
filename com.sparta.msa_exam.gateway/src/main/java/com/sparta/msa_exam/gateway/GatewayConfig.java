bfda955f6ea17e2582757f7daa0243ef521eccce
krystal-13 <honey9163@gmail.com>
2024-08-10 20:51:43 +0900

    feat: Jwt 인증 구현

---
+++ com.sparta.msa_exam.gateway/src/main/java/com/sparta/msa_exam/gateway/GatewayConfig.java
@@ -0,0 +1,31 @@
+package com.sparta.msa_exam.gateway;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+import java.security.interfaces.RSAPublicKey;
+
+@EnableWebFluxSecurity
+@Configuration
+public class GatewayConfig {
+
+    @Value("${jwt.public.key}")
+    private RSAPublicKey key;
+
+    @Bean
+    public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) {
+        http.csrf(ServerHttpSecurity.CsrfSpec::disable);
+        return http.build();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withPublicKey(this.key).build();
+    }
+}
---
+++ com.sparta.msa_exam.gateway/src/main/java/com/sparta/msa_exam/gateway/JwtAuthenticationFilter.java
@@ -0,0 +1,49 @@
+package com.sparta.msa_exam.gateway;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtException;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter implements GlobalFilter {
+
+    private static final String AUTHORIZATION = "Authorization";
+    private static final String PREFIX = "Bearer ";
+
+    private final JwtDecoder jwtDecoder;
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        String path = exchange.getRequest().getURI().getPath();
+        if (path.equals("/auth/signIn") || path.equals("/auth/signUp")) {
+            return chain.filter(exchange);
+        }
+
+        String token = extractToken(exchange);
+
+        try {
+            jwtDecoder.decode(token);
+        } catch (JwtException e) {
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete();
+        }
+
+        return chain.filter(exchange);
+    }
+
+    private String extractToken(ServerWebExchange exchange) {
+        String authHeader = exchange.getRequest().getHeaders().getFirst(AUTHORIZATION);
+        if (authHeader != null && authHeader.startsWith(PREFIX)) {
+            return authHeader.substring(PREFIX.length());
+        }
+        return null;
+    }
+}
